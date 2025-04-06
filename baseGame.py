21d6b37fe8326a754b79ab66b557da0d1b454c96
brandon-renner <brenner3@kent.edu>
2025-04-06 18:25:53 -0400

    Update baseGame.py

--- baseGame.py
+++ baseGame.py
@@ -8,21 +8,22 @@ import random
 posx = 300
 posy = 200
 
-
 def GameThread():
     pygame.init()
     #initializing colors
     background = (204, 230, 255)
     shapeColor = (0, 51, 204)
+    floorColor = (0, 0, 0)
     COLORS = [(255, 95, 31), (77, 77, 255), (57, 255, 20)]
     fallObj = []
     global posx
     global posy
     initSpeed = 3
+    bucketSize = 50
     
     fps = pygame.time.Clock()
     
-    screen_size = screen_width, screen_height = 600, 800
+    screen_size = screen_width, screen_height = 450, 600
     screen = pygame.display.set_mode(screen_size)
     pygame.display.set_caption('Welcome to CCN games')
     
@@ -32,17 +33,18 @@ def GameThread():
         rect = pygame.Rect(x_pos, 0, 20, 20)
         return rect, color
     
-    
     for n in range(COLORS.__len__()):
         rect, color = makeShapes()
         fallObj.append((rect, color))
         
-        
-    #making all shapes
-    rectBucket = pygame.Rect(0, 0, 50, 50)
-    
+    #making bucket, floor shapes
+    rectBucket = pygame.Rect(0, 0, bucketSize, bucketSize)
+    rectFloor = pygame.Rect(0, 0, 450, 10)
+    rectFloor.center = (screen_width/2, screen_height-5)
+
     #giving shapes a color
     colorRect =  (shapeColor)
+    colorFloor = (floorColor)
     
     #while loop for the game
     while True:
@@ -52,36 +54,18 @@ def GameThread():
                 sys.exit()
         screen.fill(background)
         
-        
-        
         #position shapes
         rectBucket.center = (posx, posy)
         #draw shapes
         pygame.draw.rect(screen, colorRect, rectBucket)
-        
-        
-        if keyboard.is_pressed('a'):
-            #client_socket.send('a'.encode())  # send message
-            posx -= 10
-            time.sleep(0.05)
-        if keyboard.is_pressed('d'):
-            #client_socket.send('d'.encode())  # send message
-            posx += 10
-            time.sleep(0.05)
-        if keyboard.is_pressed('s'):
-            #client_socket.send('s'.encode())  # send message
-            posy += 10
-            time.sleep(0.05)
-        if keyboard.is_pressed('w'):
-            #client_socket.send('w'.encode())  # send message
-            posy -= 10
-            time.sleep(0.05)
-            
+        pygame.draw.rect(screen, colorFloor, rectFloor)
+        collision = rectBucket.colliderect(rectFloor)
+
         #drawing falling rect
         for rect, color in fallObj:
             pygame.draw.rect(screen, color, rect)
             
-        #collision 
+        #collision of falling objects
         for rect, color in list (fallObj):
             rect.y += initSpeed
             if rect.colliderect(rectBucket):
@@ -89,15 +73,24 @@ def GameThread():
             elif rect.y > screen_height:
                 pygame.quit()
                 sys.exit()
-                
-            
-
-            
-            
-            
-            
-            
-            
+        
+        if keyboard.is_pressed('a') and posx >= (bucketSize/2):
+            #client_socket.send('a'.encode())  # send message
+            posx -= 10
+            time.sleep(0.05)
+        if keyboard.is_pressed('d') and posx <= screen_width-(bucketSize/2):
+            #client_socket.send('d'.encode())  # send message
+            posx += 10
+            time.sleep(0.05)
+        if keyboard.is_pressed('s') and not collision:
+            #client_socket.send('s'.encode())  # send message
+            posy += 10
+            time.sleep(0.05)
+        if keyboard.is_pressed('w') and posy >= (bucketSize/2):
+            #client_socket.send('w'.encode())  # send message
+            posy -= 10
+            time.sleep(0.05)
+           
         pygame.display.update()
         fps.tick(60)
     pygame.quit()
